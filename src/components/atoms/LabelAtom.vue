234b008f1ecadd3e04ec40745670aca0313725b0
VanessaMaria02 <wi21b004@technikum-wien.at>
2023-11-11 17:55:40 +0100

    project start

---
+++ src/components/atoms/LabelAtom.vue
@@ -0,0 +1,18 @@
+<template>
+    <label :for="htmlFor">
+      <slot></slot>
+    </label>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LabelAtom',
+    props: {
+      htmlFor: String,
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ src/components/molecules/FaqComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="faq" @click="toggleAnswer">
+      <h3>{{ question }}</h3>
+      <transition name="fade">
+        <p v-if="showAnswer">{{ answer }}</p>
+      </transition>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        showAnswer: false,
+      };
+    },
+    props: {
+      question: {
+        type: String,
+        required: true,
+      },
+      answer: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      toggleAnswer() {
+        this.showAnswer = !this.showAnswer;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Hier kannst du das Styling für die FAQ hinzufügen */
+  .faq {
+    margin-bottom: 15px;
+    cursor: pointer;
+  }
+  
+  h3 {
+    margin-bottom: 5px;
+    color: #41b883;
+  }
+  
+  p {
+    color: #333;
+  }
+  
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
+  
+  
